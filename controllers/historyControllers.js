a0b6a7c3922e7cf2ed2ca42f05aeb32920347671
khalil kraiem <khalil.kraiem@vegagroup.com.tn>
2022-07-27 15:06:27 +0100

    history controllers

--- controllers/historyControllers.js
+++ controllers/historyControllers.js
@@ -9,29 +9,43 @@ const addNewTransaction=(req,res)=>{
 }
 
 const selectAllTransactionsByUserId=(req,res)=>{
-    db.query(`select * transactions where userId=${req.params.userId} `,req.body,(err,result)=>{
+    db.query(`select * from transactions where userId=${req.params.userId} `,req.body,(err,result)=>{
         if (err)res.send(['errSQL',err])
         if (result)res.send([`historytransaction of userId=${req.params.userId}`,result])
     })
 }
 
 const selectTransactionByUserIdAndProviderId=(req,res)=>{
-    db.query(`select * transactions where userId=${req.params.userId} and providerId=${req.params.providerId} `,req.body,(err,result)=>{
+    db.query(`select * from transactions where userId=${req.params.userId} and providerId=${req.params.providerId} `,req.body,(err,result)=>{
         if (err)res.send(['errSQL',err])
         if (result)res.send([`historytransaction of userId=${req.params.userId} and providerId=${req.params.providerId}`,result])
     })
 
 }
 const selectTransactionByUserIdAndEmployeeId=(req,res)=>{
-    db.query(`select * transactions where userId=${req.params.userId} and employeeId=${req.params.employeeId} `,req.body,(err,result)=>{
+    db.query(`select * from transactions where userId=${req.params.userId} and employeeId=${req.params.employeeId} `,req.body,(err,result)=>{
         if (err)res.send(['errSQL',err])
         if (result)res.send([`historytransaction of userId=${req.params.userId} and employeeId=${req.params.employeeId}`,result])
     })
 }
+const selectTransactionByUserIdAndEmployeeIdAndMonth=(req,res)=>{
+    
+    
+    db.query(`select * from transactions where userId=${req.params.userId} and employeeId=${req.params.employeeId} and year(transactionDate) = year(${req.body.date})`,req.body,(err,result)=>{
+        if (err)res.send(['errSQL',err])
+        if (result)res.send([`historytransaction of userId=${req.params.userId} and employeeId=${req.params.employeeId} and month=${req.body.date}`,result])
+    })
+}
 const selectTransactionByUserIdAndClientId=(req,res)=>{
-    db.query(`select * transactions where userId=${req.params.userId} and clientId=${req.params.clientId} `,req.body,(err,result)=>{
+    db.query(`select * from transactions where userId=${req.params.userId} and clientId=${req.params.clientId} `,req.body,(err,result)=>{
         if (err)res.send(['errSQL',err])
         if (result)res.send([`historytransaction of userId=${req.params.userId} and clientId=${req.params.clientId}`,result])
+    })
+}
+const selectTransactionByUserIdAndProductId=(req,res)=>{
+    db.query(`select * from transactions t inner join products p on t.productId=p.productId inner join clients c on t.clientId=c.clientId where t.userId=${req.params.userId} and t.productId=${req.params.productId} `,req.body,(err,result)=>{
+        if (err)res.send(['errSQL',err])
+        if (result)res.send([`historytransaction of userId=${req.params.userId} and productId=${req.params.productId}`,result])
     })
 }
 module.exports={
@@ -42,6 +56,9 @@ module.exports={
     selectTransactionByUserIdAndProviderId,
 
     selectTransactionByUserIdAndEmployeeId,
+    selectTransactionByUserIdAndEmployeeIdAndMonth,
 
-    selectTransactionByUserIdAndClientId
+    selectTransactionByUserIdAndClientId,
+
+    selectTransactionByUserIdAndProductId
 }
--- routes/historyRouter.js
+++ routes/historyRouter.js
@@ -4,9 +4,13 @@ const controllers=require('../controllers/historyControllers')
 /* history selling routes */
 
 router.post('/addNewTransaction',controllers. addNewTransaction)
-router.get('/selectAllTransactionsByUserId/:userId',controllers.selectAllTransactionsByUserId)
-router.get('/selectTransactionByUserIdAndProviderId/:userId/:providerId',controllers.selectTransactionByUserIdAndProviderId)
-router.get('/selectTransactionByUserIdAndEmployeeId/:userId/:employeeId',controllers.selectTransactionByUserIdAndEmployeeId)
-router.get('/selectTransactionByUserIdAndClientId/:userId/:clientId',controllers.selectTransactionByUserIdAndClientId)
 
+router.get('/selectAllTransactionsByUserId/:userId',controllers.selectAllTransactionsByUserId)
+router.get('/selectTransactionsByUserIdAndProviderId/:userId/:providerId',controllers.selectTransactionByUserIdAndProviderId)
+
+router.get('/selectTransactionsByUserIdAndEmployeeId/:userId/:employeeId',controllers.selectTransactionByUserIdAndEmployeeId)
+router.post('/selectTransactionsByUserIdAndEmployeeIdAndMonth/:userId/:employeeId',controllers.selectTransactionByUserIdAndEmployeeIdAndMonth)
+
+router.get('/selectTransactionsByUserIdAndClientId/:userId/:clientId',controllers.selectTransactionByUserIdAndClientId)
+router.get('/selectTransactionsByUserIdAndproductId/:userId/:productId',controllers.selectTransactionByUserIdAndProductId)
 module.exports=router
