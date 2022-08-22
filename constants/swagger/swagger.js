2663d18acaa6876f77798c7c13ccd610b39fd663
khalil kraiem <khalil.kraiem@vegagroup.com.tn>
2022-08-22 15:31:37 +0100

    swagger fist test

---
+++ constants/swagger/swagger.js
@@ -0,0 +1,27 @@
+/**
+ * @swagger
+ * /api/user/getAllUser:
+ *   get:
+ *     description: Get all users
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+
+/**
+ * @swagger
+ * /api/user/getAll:
+ *   get:
+ *     description: Get all users
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+
+
+
+
+
+
+
+
