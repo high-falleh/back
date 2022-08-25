84ae2c56f594f2ce44d6dbeaf93ec5e12b516318
khalil kraiem <khalil.kraiem@vegagroup.com.tn>
2022-08-25 15:19:30 +0100

    swagger of user with body parameters

--- constants/swagger/userSwagger.js
+++ constants/swagger/userSwagger.js
@@ -4,6 +4,7 @@
  *  get:
  *      tags:
  *        - User
+ *      summary: get all users.
  *      description: Get all users
  *      responses:
  *       200:
@@ -16,55 +17,136 @@
  *  post:
  *      tags:
  *        - User
+ *      summary: create new user.
  *      description: register
+ *      consumes: 
+ *          -application/json
+ *      parameters:
+ *           - in: body
+ *             required: true
+ *             name: user
+ *             schema:
+ *                  type: object
+ *                  required:
+ *                      - userEmail
+ *                      - userFullName
+ *                      - userPassword
+ *                  properties:
+ *                      userFullName:
+ *                          type: string
+ *                          default: khalil kraiem
+ *                      userEmail: 
+ *                          type: string
+ *                          default: khalilkraiemmedia@gmail.com
+ *                      userPassword: 
+ *                          type: string
+ *                          default: 11019029
+ *                      userDateOfBirth:
+ *                          type: date
+ *                          default: 1992-10-21
+ *                      userPhone:
+ *                          type: string
+ *                          default: 52701262
  *      responses:
- *       200:
- *         description: Success
+ *          200:
+ *              description: Success
  */
 
 /**
  * @swagger
  * /api/user/login:
- *  post:
+*  post:
  *      tags:
  *        - User
- *      description: Login 
- *      responses:
- *       200:
- *         description: Success
- */
-
-/**
- * @swagger
- * /api/user/updatePassword/{userId}:
- *  put:
- *      tags:
- *        - User
- *      
+ *      summary: login.
+ *      description: login
+ *      consumes: 
+ *          -application/json
  *      parameters:
- *      - name: userId
- *        in: path
- *        type: integer
- *        description: The id of the user
- *        required: true
- *      description: update Password
+ *           - in: body
+ *             required: true
+ *             name: user
+ *             schema:
+ *                  type: object
+ *                  required:
+ *                      - userEmail
+ *                      - userPassword
+ *                  properties:
+ *                      userEmail: 
+ *                          type: string
+ *                          default: khalilkraiemmedia@gmail.com
+ *                      userPassword: 
+ *                          type: string
+ *                          default: 11019029
  *      responses:
- *       200:
- *         description: Success
+ *          200:
+ *              description: Success
+ */
+
+
+/**
+ * @swagger
+ * /api/user/updatePassword/:userId:
+ *  put:
+ *      tags:
+ *        - User
+ *      summary: update Password.
+ *      description: update Password
+ *      parameters:
+ *          - in: path
+ *            name: userId
+ *            type: integer
+ *            description: The id of the user
+ *            required: true
+ *          - in: body
+ *            required: true
+ *            schema:
+ *              type: object
+ *              required:
+ *                  - userEmail
+ *                  - userPassword
+ *              properties:
+ *                  current:
+ *                      type: string
+ *                      default: 11019029
+ *                  newPassword:
+ *                      type: string
+ *                      default: 11019029
+ * 
+ *      responses:
+ *          200:
+ *              description: Success
  */
 
 /**
  * @swagger
- * /api/user/updateEmail/{userId}:
+ * /api/user/updateEmail/:userId:
  *  put:
  *      tags:
  *        - User
+ *      summary: update Email.
  *      description: update Email
+ *      parameters:
+ *          - in: path
+ *            name: userId
+ *            type: integer
+ *            description: The id of the user
+ *            required: true
+ *          - in: body
+ *            required: true
+ *            schema:
+ *              type: object
+ *              required:
+ *                  - newEmail
+ *              properties:
+ *                  newEmail:
+ *                      type: string
+ *                      default: m.khalilkraiem@gmail.com
+ * 
  *      responses:
- *       200:
- *         description: Success
+ *          200:
+ *              description: Success
  */
-
 
 
 
